54ca0f71d30cadce0287300d2462bcacb3c90be0
Rachael Ghorbani <rachael.ghorbani@gmail.com>
2020-07-15 15:48:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,7 +4,24 @@ def game_hash
               :team_name => "Brooklyn Nets",
               :colors => ["Black", "White"],
               :players => [
-                          {:players_name =>
+                         {:players_name => "Alan Anderson",
+                          :number => 0,
+                          :shoe => 16,
+                          :points => 22,
+                          :rebounds => 12,
+                          :assists => 12,
+                          :steals => 3,
+                          :blocks => 1,
+                          :slam_dunks => 1},
+                         {:players_name => "Reggie Evans",
+                          :number => 30,
+                          :shoe => 14,
+                          :points => 12,
+                          :rebounds => 12,
+                          :assists => 12,
+                          :steals => 12,
+                          :blocks => 12,
+                          :slam_dunks => 1},
                 
                 
                 
