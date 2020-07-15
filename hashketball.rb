cbe9a7c9a569dd4e0446768659b30df6b855bd33
Rachael Ghorbani <rachael.ghorbani@gmail.com>
2020-07-15 15:51:54 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -13,6 +13,7 @@ def game_hash
                           :steals => 3,
                           :blocks => 1,
                           :slam_dunks => 1},
+                          
                          {:players_name => "Reggie Evans",
                           :number => 30,
                           :shoe => 14,
@@ -21,12 +22,40 @@ def game_hash
                           :assists => 12,
                           :steals => 12,
                           :blocks => 12,
-                          :slam_dunks => 1},
-                
-                
-                
-                ]
-    }
+                          :slam_dunks => 7},
+                          
+                         {:players_name => "Brook Lopez",
+                          :number => 11,
+                          :shoe => 17,
+                          :points => 17,
+                          :rebounds => 19,
+                          :assists => 10,
+                          :steals => 3,
+                          :blocks => 1,
+                          :slam_dunks => 15},
+                          
+                         {:players_name => "Mason Plumlee",
+                          :number => 1,
+                          :shoe => 19,
+                          :points => 26,
+                          :rebounds => 11,
+                          :assists => 6,
+                          :steals => 3,
+                          :blocks => 8,
+                          :slam_dunks => 5},
+                        
+                         {:players_name => "Jason Terry",
+                          :number => 31,
+                          :shoe => 15,
+                          :points => 19,
+                          :rebounds => 2,
+                          :assists => 2,
+                          :steals => 4,
+                          :blocks => 11,
+                          :slam_dunks => 1}
+                                            ]
+                                              },
+    :away
     
   }
   hashketball
